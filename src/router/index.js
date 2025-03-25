81227a701d7f927d6288983d626e2c7ab2e71bbf
nelamine <imansurova72@gmail.com>
2025-03-26 02:29:54 +0500

    add page NotFound

--- src/router/index.js
+++ src/router/index.js
@@ -2,13 +2,14 @@ import { createRouter, createWebHistory } from 'vue-router';
 import Settings from '../components/Settings.vue';
 import Game from '../components/GameBoard.vue';
 import Leaderboard from '../components/LeaderBoard.vue';
+import NotFound from '../components/NotFound.vue'
 
 const routes = [
   { path: '/', redirect: '/settings' },
   { path: '/settings', component: Settings },
   { path: '/game', component: Game },
   { path: '/leaderboard', component: Leaderboard },
-  // добавить страницу ошибки 404
+  { path: '/:pathMatch(.*)*', component: NotFound },
 ];
 
 const router = createRouter({
